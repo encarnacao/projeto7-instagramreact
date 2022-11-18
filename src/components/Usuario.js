c87793e4819b88830bbbe7f36228da61f0618a40
encarnacao <cencarnacaoq@gmail.com>
2022-11-18 20:36:32 -0300

    Finished layout and added functions to change name and avatar

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,10 +1,23 @@
+import {IoPencil} from 'react-icons/io5';
+import React from 'react';
+
 export default function User() {
+    const [name,setName] = React.useState("Caio Encarnação");
+    const [avatar,setAvatar] = React.useState("assets/avatars/eu.jpg");
+    function changeName(){
+        const newName = prompt("Digite seu nome:")
+        return newName?setName(newName):null;
+    }
+    function changeAvatar(){
+        const newAvatar = prompt("Digite o link da imagem:")
+        return newAvatar?setAvatar(newAvatar):null;
+    }
     return (
         <div className="meu-perfil">
-            <a href="https://instagram.com/cencarnacaoq"><img src="assets/avatars/eu.jpg" alt="" className="perfil-img" /></a>
+            <a href="#link"><img src={avatar} alt="" onClick={changeAvatar} className="perfil-img" /></a>
             <div className="nome">
                 <a href="https://instagram.com/cencarnacaoq"><h1>cencarnacaoq</h1></a>
-                <p>Caio Encarnação</p>
+                <p>{name}<span style={{cursor:"pointer"}} onClick={changeName}><IoPencil /></span></p>
             </div>
         </div>
     );
